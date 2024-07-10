c27bf586058b14abd253b5ecc0bcfa2acf04ea35
Ethan <ethanokamura3@gmail.com>
2024-07-09 19:13:51 -0700

    added new routes

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,5 +1,5 @@
 // pages
-import 'package:rando/pages/post.dart';
+import 'package:rando/pages/create/create_item.dart';
 import 'package:rando/pages/profile/create_profile.dart';
 import 'package:rando/pages/profile/edit_profile.dart';
 import 'package:rando/pages/login.dart';
@@ -8,7 +8,7 @@ import 'package:rando/pages/home.dart';
 var appRoutes = {
   '/': (context) => const HomeScreen(),
   '/login': (context) => LoginScreen(),
-  '/post': (context) => PostScreen(),
+  '/post': (context) => const CreateItemScreen(),
   '/edit_profile': (context) => const EditProfileScreen(),
   '/create_profile': (context) => const CreateProfilePage(),
 };

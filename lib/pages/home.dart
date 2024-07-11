117353070e80967c575de850b9d70dc8ab823a97
Ethan <ethanokamura3@gmail.com>
2024-07-11 10:58:58 -0700

    added actual homepage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,38 +1,65 @@
 // dart packages
 import 'package:flutter/material.dart';
-
-// utils
-import 'package:rando/services/auth.dart';
-
-// pages
-import 'package:rando/pages/login.dart';
-import 'package:rando/pages/lists.dart';
+import 'package:provider/provider.dart';
+import 'package:rando/components/bottom_nav.dart';
+import 'package:rando/utils/theme/theme_provider.dart';
 
 // show list screen if logged in otherwise show sign in page
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: AuthService().userStream,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          // show loading widget
-          return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          // show error widget
-          return const Center(child: Text('error'));
-        } else if (snapshot.hasData) {
-          // logged in
-          final user = AuthService().user;
-          if (user == null) return LoginScreen();
-          return ListScreen(userID: user.uid);
-        } else {
-          // not logged in
-          return LoginScreen();
-        }
-      },
+    ThemeProvider themeProvider = Provider.of<ThemeProvider>(context);
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Icon(Icons.person),
+                  ),
+                  Text(
+                    "LocalsOnly",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  Switch(
+                    value: themeProvider.isDarkMode,
+                    onChanged: (value) => themeProvider.toggleTheme(),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Center(
+                  child: Container(
+                    height: 128,
+                    width: 128,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      image: const DecorationImage(
+                        image: AssetImage('assets/images/localsonly_face.png'),
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: const BottomNavBar(),
     );
   }
 }

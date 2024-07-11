d37027a94ca1a22ef7f0bcddc958c17765ddd9d7
Ethan <ethanokamura3@gmail.com>
2024-07-11 12:36:26 -0700

    added more pages

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,16 +1,24 @@
 // pages
-import 'package:rando/pages/create/create_item.dart';
+import 'package:rando/pages/create/create_activity.dart';
+import 'package:rando/pages/create/create_board.dart';
+import 'package:rando/pages/create/select_create.dart';
 import 'package:rando/pages/profile/create_profile.dart';
 import 'package:rando/pages/profile/edit_profile.dart';
 import 'package:rando/pages/login.dart';
 import 'package:rando/pages/home.dart';
+import 'package:rando/pages/profile/my_profile.dart';
+import 'package:rando/pages/profile/user_settings.dart';
 import 'package:rando/pages/router.dart';
 
 var appRoutes = {
   '/': (context) => const RouterWidget(),
-  '/home': (context) => HomeScreen(),
+  '/home': (context) => const HomeScreen(),
   '/login': (context) => LoginScreen(),
-  '/post': (context) => const CreateItemScreen(),
+  '/create': (context) => const SelectCreateScreen(),
+  '/create-activity': (context) => const CreateActivityScreen(),
+  '/create-board': (context) => const CreateBoardScreen(),
+  '/my_profile': (context) => const MyProfileScreen(),
+  '/user_settings': (context) => const UserSettingsScreen(),
   '/edit_profile': (context) => const EditProfileScreen(),
   '/create_profile': (context) => const CreateProfilePage(),
 };

1144e5c22cdf4e2e39a2f2bde32d23a559cd23af
Ethan <ethanokamura3@gmail.com>
2024-07-11 10:58:46 -0700

    added actual homepage

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -4,9 +4,11 @@ import 'package:rando/pages/profile/create_profile.dart';
 import 'package:rando/pages/profile/edit_profile.dart';
 import 'package:rando/pages/login.dart';
 import 'package:rando/pages/home.dart';
+import 'package:rando/pages/router.dart';
 
 var appRoutes = {
-  '/': (context) => const HomeScreen(),
+  '/': (context) => const RouterWidget(),
+  '/home': (context) => HomeScreen(),
   '/login': (context) => LoginScreen(),
   '/post': (context) => const CreateItemScreen(),
   '/edit_profile': (context) => const EditProfileScreen(),

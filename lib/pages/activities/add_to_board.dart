83c79a85383f35d0cd69ec9e797ca192d1a10cd3
Ethan <ethanokamura3@gmail.com>
2024-07-15 23:00:19 -0700

    setting up tasks

---
+++ lib/pages/activities/add_to_board.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// TODO
+/// Grab a future of the list of boards,
+/// have a check list to see if the item is inside the board
+/// add or remove item from boards
+
+class AddToBoardScreen extends StatelessWidget {
+  final String userID;
+  const AddToBoardScreen({super.key, required this.userID});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

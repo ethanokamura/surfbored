eca0766ba418e18fbc37ade4e3e415e67c8a367c
Ethan <ethanokamura3@gmail.com>
2024-07-17 18:25:04 -0700

    general create page

--- lib/pages/create.dart
+++ lib/pages/create.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:rando/components/create/create_object.dart';
-import 'package:rando/components/tab_bar/tab.dart';
-import 'package:rando/components/tab_bar/tab_bar.dart';
+// import 'package:rando/components/tab_bar/tab.dart';
+// import 'package:rando/components/tab_bar/tab_bar.dart';
 
-class CreateObjectScreen extends StatelessWidget {
-  const CreateObjectScreen({super.key});
-
-  final String type = 'items';
+class CreateScreen extends StatelessWidget {
+  final String type;
+  const CreateScreen({super.key, required this.type});
 
   @override
   Widget build(BuildContext context) {
@@ -14,79 +13,17 @@ class CreateObjectScreen extends StatelessWidget {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text("Create Something!"),
+          title: type == 'items'
+              ? const Text("Create An Activity!")
+              : const Text("Create A Board!"),
         ),
         body: SafeArea(
-          child: buildCreateScreen(context),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: CreateObjectWidget(type: type),
+          ),
         ),
       ),
     );
   }
-}
-
-Widget buildCreateScreen(BuildContext context) {
-  return const CustomScrollView(
-    slivers: [
-      SliverSafeArea(
-        sliver: SliverToBoxAdapter(
-          child: Padding(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                CustomTabBarWidget(
-                  tabs: [
-                    CustomTabWidget(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.photo_library_outlined,
-                            size: 20,
-                          ),
-                          SizedBox(width: 10),
-                          Text(
-                            'Activity',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ),
-                    CustomTabWidget(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.list,
-                            size: 20,
-                          ),
-                          SizedBox(width: 10),
-                          Text(
-                            'Board',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      SliverFillRemaining(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: TabBarView(
-            children: [
-              CreateObjectWidget(type: "items"),
-              CreateObjectWidget(type: "boards"),
-            ],
-          ),
-        ),
-      ),
-    ],
-  );
 }

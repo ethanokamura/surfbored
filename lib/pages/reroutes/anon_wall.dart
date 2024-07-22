f0c3222232353d0096de22a374a05d8ec0949e76
Ethan <ethanokamura3@gmail.com>
2024-07-21 18:47:02 -0700

    changed profile page names

--- lib/pages/reroutes/anon_wall.dart
+++ lib/pages/reroutes/anon_wall.dart
@@ -1,36 +1,36 @@
-import 'package:flutter/material.dart';
-import 'package:rando/shared/widgets/buttons/defualt_button.dart';
-import 'package:rando/core/services/auth_service.dart';
+// import 'package:flutter/material.dart';
+// import 'package:rando/shared/widgets/buttons/defualt_button.dart';
+// import 'package:rando/core/services/auth_service.dart';
 
-class AnonWallWidget extends StatelessWidget {
-  final String message;
-  const AnonWallWidget({super.key, required this.message});
+// class AnonWallWidget extends StatelessWidget {
+//   final String message;
+//   const AnonWallWidget({super.key, required this.message});
 
-  // logout user
-  void logOut(BuildContext context) async {
-    await AuthService.signOut();
-    // reset navigation stack
-    if (context.mounted) {
-      Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
-    }
-  }
+//   // logout user
+//   void logOut(BuildContext context) async {
+//     await AuthService.signOut();
+//     // reset navigation stack
+//     if (context.mounted) {
+//       Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
+//     }
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(message),
-            DefualtButton(
-              inverted: true,
-              onTap: () => logOut(context),
-              text: "Login",
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Center(
+//         child: Column(
+//           mainAxisSize: MainAxisSize.min,
+//           children: [
+//             Text(message),
+//             DefualtButton(
+//               inverted: true,
+//               onTap: () => logOut(context),
+//               text: "Login",
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }

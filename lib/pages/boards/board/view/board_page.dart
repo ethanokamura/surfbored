930865f0c01ca85455ba68079361dcfcc712ffd6
Ethan <ethanokamura3@gmail.com>
2024-07-25 17:28:18 -0700

    simplified board page

--- lib/pages/boards/board/view/board_page.dart
+++ lib/pages/boards/board/view/board_page.dart
@@ -2,9 +2,11 @@ import 'package:app_ui/app_ui.dart';
 import 'package:boards_repository/boards_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rando/pages/boards/board/view/board_items.dart';
+import 'package:items_repository/items_repository.dart';
+import 'package:rando/pages/activities/activities.dart';
 import 'package:rando/pages/boards/cubit/board_cubit.dart';
 import 'package:rando/pages/boards/edit_board/edit_board.dart';
+import 'package:rando/pages/boards/shared/board_activities/board_activities.dart';
 import 'package:rando/pages/boards/shuffle/shuffle_items.dart';
 import 'package:user_repository/user_repository.dart';
 
@@ -23,9 +25,7 @@ class BoardPage extends StatelessWidget {
       create: (context) => BoardCubit(
         boardsRepository: context.read<BoardsRepository>(),
         userRepository: context.read<UserRepository>(),
-      )..streamBoard(boardID).listen((state) {
-          context.read<BoardCubit>();
-        }),
+      )..streamBoard(boardID),
       child: Scaffold(
         body: BlocBuilder<BoardCubit, BoardState>(
           builder: (context, state) {
@@ -34,138 +34,7 @@ class BoardPage extends StatelessWidget {
             } else if (state is BoardLoaded) {
               final board = state.board;
               final user = context.read<BoardCubit>().getUser();
-              final isOwner = context.read<BoardCubit>().isOwner(
-                    board.uid,
-                    user.uid,
-                  );
-              final likedByUser = user.hasLikedBoard(boardID: board.id);
-              return SingleChildScrollView(
-                child: SafeArea(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      left: defaultPadding,
-                      right: defaultPadding,
-                      bottom: defaultPadding,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            ActionButton(
-                              horizontal: 0,
-                              vertical: 0,
-                              inverted: false,
-                              onTap: () => Navigator.pop(context),
-                              icon: Icons.arrow_back_ios_new_rounded,
-                            ),
-                            const SizedBox(width: 5),
-                            Expanded(
-                              child: AutoSizeText(
-                                '${board.title}:',
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                textDirection: TextDirection.ltr,
-                                textAlign: TextAlign.center,
-                                style: const TextStyle(
-                                  fontSize: 26,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(width: 5),
-                            ActionButton(
-                              horizontal: 0,
-                              vertical: 0,
-                              inverted: false,
-                              onTap: () {}, // settings
-                              icon: Icons.more_horiz,
-                            ),
-                          ],
-                        ),
-                        const VerticalSpacer(),
-                        ImageWidget(
-                          borderRadius: defaultBorderRadius,
-                          photoURL: board.photoURL,
-                          height: 256,
-                          width: double.infinity,
-                        ),
-                        const VerticalSpacer(),
-                        Text(
-                          board.title,
-                          style: TextStyle(
-                            color: Theme.of(context).textColor,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          board.description,
-                          style: TextStyle(
-                            color: Theme.of(context).subtextColor,
-                          ),
-                        ),
-                        Text(
-                          '@${user.username}',
-                          style: TextStyle(
-                            color: Theme.of(context).accentColor,
-                          ),
-                        ),
-                        const VerticalSpacer(),
-                        Row(
-                          children: [
-                            if (isOwner)
-                              Expanded(
-                                child: ActionButton(
-                                  inverted: false,
-                                  onTap: () => Navigator.push(
-                                    context,
-                                    MaterialPageRoute<dynamic>(
-                                      builder: (context) =>
-                                          EditBoardPage(boardID: board.id),
-                                    ),
-                                  ),
-                                  text: 'Edit Board',
-                                ),
-                              )
-                            else
-                              Expanded(
-                                child: LikeButton(
-                                  likes: board.likes,
-                                  isLiked: likedByUser,
-                                  onTap: () =>
-                                      context.read<BoardCubit>().toggleLike(
-                                            user.uid,
-                                            board.id,
-                                            likedByUser,
-                                          ),
-                                ),
-                              ),
-                            const HorizontalSpacer(),
-                            Expanded(
-                              child: ActionButton(
-                                inverted: false,
-                                onTap: () => Navigator.push(
-                                  context,
-                                  MaterialPageRoute<dynamic>(
-                                    builder: (context) =>
-                                        ShuffleItemScreen(boardID: board.id),
-                                  ),
-                                ),
-                                text: 'Shuffle',
-                              ),
-                            ),
-                          ],
-                        ),
-                        const VerticalSpacer(),
-                        BoardItems(boardID: board.id),
-                      ],
-                    ),
-                  ),
-                ),
-              );
+              return BoardPageView(board: board, user: user);
             } else if (state is BoardError) {
               return Center(child: Text(state.message));
             } else {
@@ -174,6 +43,227 @@ class BoardPage extends StatelessWidget {
           },
         ),
       ),
+    );
+  }
+}
+
+class BoardPageView extends StatelessWidget {
+  const BoardPageView({
+    required this.board,
+    required this.user,
+    super.key,
+  });
+  final Board board;
+  final User user;
+  @override
+  Widget build(BuildContext context) {
+    final isOwner = context.read<BoardCubit>().isOwner(
+          board.uid,
+          user.uid,
+        );
+    return BlocProvider(
+      create: (context) => ItemCubit(
+        itemsRepository: context.read<ItemsRepository>(),
+        userRepository: context.read<UserRepository>(),
+        boardsRepository: context.read<BoardsRepository>(),
+      )..streamBoardItems(board.id),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: defaultPadding,
+            right: defaultPadding,
+            bottom: defaultPadding,
+          ),
+          child: NestedScrollView(
+            headerSliverBuilder: (context, _) {
+              return [
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TopBar(title: board.title),
+                          const VerticalSpacer(),
+                          ImageWidget(
+                            borderRadius: defaultBorderRadius,
+                            photoURL: board.photoURL,
+                            height: 256,
+                            width: double.infinity,
+                          ),
+                          const VerticalSpacer(),
+                          BoardDetails(
+                            title: board.description,
+                            description: board.description,
+                            username: user.username,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ];
+            },
+            body: Column(
+              children: [
+                const VerticalSpacer(),
+                BoardButtons(
+                  isOwner: isOwner,
+                  user: user,
+                  board: board,
+                ),
+                const VerticalSpacer(),
+                Flexible(
+                  child: BoardActivities(boardID: board.id),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TopBar extends StatelessWidget {
+  const TopBar({required this.title, super.key});
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        ActionButton(
+          horizontal: 0,
+          vertical: 0,
+          inverted: false,
+          onTap: () => Navigator.pop(context),
+          icon: Icons.arrow_back_ios_new_rounded,
+        ),
+        const SizedBox(width: 5),
+        Expanded(
+          child: AutoSizeText(
+            '$title:',
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            textDirection: TextDirection.ltr,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const SizedBox(width: 5),
+        ActionButton(
+          horizontal: 0,
+          vertical: 0,
+          inverted: false,
+          onTap: () {}, // settings
+          icon: Icons.more_horiz,
+        ),
+      ],
+    );
+  }
+}
+
+class BoardDetails extends StatelessWidget {
+  const BoardDetails({
+    required this.title,
+    required this.description,
+    required this.username,
+    super.key,
+  });
+  final String title;
+  final String description;
+  final String username;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      // mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            color: Theme.of(context).textColor,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          '@$username',
+          style: TextStyle(
+            color: Theme.of(context).accentColor,
+          ),
+        ),
+        Text(
+          description,
+          style: TextStyle(
+            color: Theme.of(context).subtextColor,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class BoardButtons extends StatelessWidget {
+  const BoardButtons({
+    required this.isOwner,
+    required this.user,
+    required this.board,
+    super.key,
+  });
+  final bool isOwner;
+  final Board board;
+  final User user;
+  @override
+  Widget build(BuildContext context) {
+    final likedByUser = user.hasLikedBoard(boardID: board.id);
+    return Row(
+      children: [
+        if (isOwner)
+          Expanded(
+            child: ActionButton(
+              inverted: false,
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute<dynamic>(
+                  builder: (context) => EditBoardPage(boardID: board.id),
+                ),
+              ),
+              text: 'Edit Board',
+            ),
+          )
+        else
+          Expanded(
+            child: LikeButton(
+              likes: board.likes,
+              isLiked: likedByUser,
+              onTap: () => context.read<BoardCubit>().toggleLike(
+                    user.uid,
+                    board.id,
+                    isLiked: likedByUser,
+                  ),
+            ),
+          ),
+        const HorizontalSpacer(),
+        Expanded(
+          child: ActionButton(
+            inverted: false,
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute<dynamic>(
+                builder: (context) => ShuffleItemScreen(boardID: board.id),
+              ),
+            ),
+            text: 'Shuffle',
+          ),
+        ),
+      ],
     );
   }
 }

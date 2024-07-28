2ba422140a495acd0d6c110788c5cf22cf24a190
Ethan <ethanokamura3@gmail.com>
2024-07-27 22:43:40 -0700

    fixing light mode

--- packages/app_ui/lib/src/colors.dart
+++ packages/app_ui/lib/src/colors.dart
@@ -15,9 +15,9 @@ abstract class CustomColors {
 
   // light mode
   static const Color lightAccent = Color.fromRGBO(68, 157, 230, 1);
-  static const Color lightBackground = Color.fromRGBO(220, 220, 220, 1);
-  static const Color lightSurface = Color.fromRGBO(245, 245, 245, 1);
-  static const Color lightPrimary = Color.fromRGBO(190, 190, 190, 1);
+  static const Color lightBackground = Color.fromRGBO(200, 210, 210, 1);
+  static const Color lightSurface = Color.fromRGBO(240, 250, 250, 1);
+  static const Color lightPrimary = Color.fromRGBO(220, 230, 230, 1);
   static const Color lightTextColor = Color.fromRGBO(10, 10, 10, 1);
   static const Color lightSubtextColor = Color.fromRGBO(64, 64, 64, 1);
   static const Color lightHintTextColor = Color.fromRGBO(100, 100, 100, 1);

cd06caf634980840d2e27be9bf3a2de85d95451e
Ethan <ethanokamura3@gmail.com>
2024-08-05 00:47:06 -0700

    board clean up

--- lib/features/boards/shared/shared.dart
+++ lib/features/boards/shared/shared.dart
@@ -1,5 +1,4 @@
-export 'add_to_board_page.dart';
-export 'board_card.dart';
-export 'board_posts.dart';
+export 'add_to_board/add_to_board.dart';
+export 'board_card/board_card.dart';
 export 'select_board/select_board.dart';
 export 'user_boards.dart';

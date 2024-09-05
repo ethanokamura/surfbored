fcbc225fc4983d1a9dff240d1ee21a9f42be4546
Ethan <ethanokamura3@gmail.com>
2024-09-04 19:08:20 -0700

    more options in profile

---
+++ lib/features/profile/profile/view/more_profile_options.dart
@@ -0,0 +1,95 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:flutter/material.dart';
+
+enum Options {
+  share,
+  edit,
+  block,
+}
+
+class MoreProfileOptions extends StatelessWidget {
+  const MoreProfileOptions({
+    required this.isCurrent,
+    required this.onEdit,
+    required this.onBlock,
+    required this.onShare,
+    super.key,
+  });
+  final bool isCurrent;
+  final void Function() onEdit;
+  final void Function() onBlock;
+  final void Function() onShare;
+
+  @override
+  Widget build(BuildContext context) {
+    final style = ElevatedButton.styleFrom(
+      padding: const EdgeInsets.all(8),
+      elevation: defaultElevation,
+      shadowColor: Colors.black,
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      shape: const RoundedRectangleBorder(borderRadius: defaultBorderRadius),
+    );
+    return PopupMenuButton<Options>(
+      style: style,
+      itemBuilder: (BuildContext context) => [
+        // share
+        // block
+        // settings
+
+        // share
+        const PopupMenuItem(
+          value: Options.share,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(Icons.ios_share),
+              SizedBox(width: 10),
+              PrimaryText(text: 'Share'),
+            ],
+          ),
+        ),
+        if (isCurrent)
+          const PopupMenuItem(
+            value: Options.edit,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Icons.settings),
+                SizedBox(width: 10),
+                PrimaryText(text: 'Edit'),
+              ],
+            ),
+          ),
+        // edit activity
+        if (!isCurrent)
+          const PopupMenuItem(
+            value: Options.block,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Icons.block_flipped),
+                SizedBox(width: 10),
+                PrimaryText(text: 'Block/Unblock'),
+              ],
+            ),
+          ),
+      ],
+      onSelected: (value) {
+        switch (value) {
+          // share
+          case Options.share:
+            break;
+          // settings
+          case Options.edit:
+            onEdit();
+          // block
+          case Options.block:
+            onBlock();
+        }
+      },
+    );
+  }
+}

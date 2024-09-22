b68b615242c6dce3d94eff884fd8c250aa4c7ad9
Ethan <ethanokamura3@gmail.com>
2024-09-22 13:40:47 -0700

    removed button padding

--- packages/app_ui/lib/src/button_styles.dart
+++ packages/app_ui/lib/src/button_styles.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 ButtonStyle defaultStyle(BuildContext context, {bool? onSurface}) {
   return ElevatedButton.styleFrom(
-    padding: const EdgeInsets.all(defaultPadding),
+    padding: EdgeInsets.zero,
     elevation: 0,
     backgroundColor: onSurface != null && onSurface
         ? Theme.of(context).colorScheme.primary
@@ -15,7 +15,7 @@ ButtonStyle defaultStyle(BuildContext context, {bool? onSurface}) {
 
 ButtonStyle inverseStyle(BuildContext context) {
   return ElevatedButton.styleFrom(
-    padding: const EdgeInsets.all(defaultPadding),
+    padding: EdgeInsets.zero,
     elevation: 0,
     backgroundColor: Theme.of(context).accentColor,
     shape: const RoundedRectangleBorder(borderRadius: defaultBorderRadius),
@@ -24,6 +24,7 @@ ButtonStyle inverseStyle(BuildContext context) {
 
 ButtonStyle noBackgroundStyle(BuildContext context) {
   return ElevatedButton.styleFrom(
+    padding: EdgeInsets.zero,
     backgroundColor: Colors.transparent,
     elevation: 0,
   );

f66b8f9906dfb3fc00818fb9434a8bf976d0ab41
Ethan <ethanokamura3@gmail.com>
2024-10-21 14:51:47 -0700

    added inline documentation and todos

--- packages/app_ui/lib/src/widgets.dart
+++ packages/app_ui/lib/src/widgets.dart
@@ -1,19 +1,21 @@
 import 'package:app_ui/src/constants.dart';
+import 'package:app_ui/src/extensions.dart';
 import 'package:app_ui/src/icons.dart';
 import 'package:app_ui/src/text.dart';
 import 'package:app_ui/src/theme.dart';
 import 'package:flutter/material.dart';
 
+/// Custom container widget
+/// Requires [child] widget to display inside the container
+/// Optional padding using [horizontal] and [vertical]
 class CustomContainer extends StatelessWidget {
   const CustomContainer({
     required this.child,
-    this.inverted,
     this.horizontal,
     this.vertical,
     super.key,
   });
 
-  final bool? inverted;
   final double? horizontal;
   final double? vertical;
   final Widget child;
@@ -21,10 +23,7 @@ class CustomContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: inverted != null && inverted!
-          ? Theme.of(context).accentColor
-          : Theme.of(context).colorScheme.surface,
-      shadowColor: Theme.of(context).shadowColor,
+      color: context.theme.colorScheme.surface,
       borderRadius: defaultBorderRadius,
       child: Padding(
         padding: EdgeInsets.symmetric(
@@ -37,6 +36,7 @@ class CustomContainer extends StatelessWidget {
   }
 }
 
+/// TODO(Ethan): remove?
 class CustomInputField extends StatelessWidget {
   const CustomInputField({
     required this.label,
@@ -77,6 +77,7 @@ class CustomInputField extends StatelessWidget {
   }
 }
 
+/// TODO(Ethan): remove?
 class CustomTextBox extends StatelessWidget {
   const CustomTextBox({
     required this.text,
@@ -94,7 +95,6 @@ class CustomTextBox extends StatelessWidget {
     return GestureDetector(
       onTap: onPressed,
       child: CustomContainer(
-        inverted: false,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
@@ -116,6 +116,8 @@ class CustomTextBox extends StatelessWidget {
   }
 }
 
+/// Custom container widget
+/// Requires [child] child to display inside the tab
 class CustomTabWidget extends StatelessWidget {
   const CustomTabWidget({required this.child, super.key});
   final Widget child;
@@ -126,6 +128,8 @@ class CustomTabWidget extends StatelessWidget {
   }
 }
 
+/// Custom container widget
+/// Requires [tabs] widget list to display inside the tab bar
 class CustomTabBarWidget extends StatelessWidget {
   const CustomTabBarWidget({required this.tabs, super.key});
   final List<Widget> tabs;
@@ -135,24 +139,27 @@ class CustomTabBarWidget extends StatelessWidget {
     return Container(
       height: 30,
       decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surface,
+        color: context.theme.colorScheme.surface,
         borderRadius: defaultBorderRadius,
       ),
       child: TabBar(
         indicatorSize: TabBarIndicatorSize.tab,
         dividerColor: Colors.transparent,
         indicator: BoxDecoration(
-          color: Theme.of(context).colorScheme.primary,
+          color: context.theme.colorScheme.primary,
           borderRadius: defaultBorderRadius,
         ),
-        labelColor: Theme.of(context).textColor,
-        unselectedLabelColor: Theme.of(context).subtextColor,
+        labelColor: context.theme.textColor,
+        unselectedLabelColor: context.theme.subtextColor,
         tabs: tabs,
       ),
     );
   }
 }
 
+/// Custom container widget
+/// Requires [body] content to display inside the page
+/// Optional [appBar] and [top] padding
 class CustomPageView extends StatelessWidget {
   const CustomPageView({
     required this.body,
@@ -183,6 +190,7 @@ class CustomPageView extends StatelessWidget {
   }
 }
 
+/// Customer vertical spacer with default spacing
 class VerticalSpacer extends StatelessWidget {
   const VerticalSpacer({super.key});
   @override
@@ -191,6 +199,7 @@ class VerticalSpacer extends StatelessWidget {
   }
 }
 
+/// Customer horizontal spacer with default spacing
 class HorizontalSpacer extends StatelessWidget {
   const HorizontalSpacer({super.key});
   @override

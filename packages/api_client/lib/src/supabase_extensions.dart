da7eaa4e57407fcee9e45862f501522b3a121bde
Ethan <ethanokamura3@gmail.com>
2024-11-24 20:27:21 -0800

    update images as well as upload

--- packages/api_client/lib/src/supabase_extensions.dart
+++ packages/api_client/lib/src/supabase_extensions.dart
@@ -40,4 +40,26 @@ extension SupabaseExtensions on SupabaseClient {
       throw Exception('Error uploading file $e');
     }
   }
+
+  /// Update An Existing File
+  /// [collection] the collection the file is stored in
+  /// [file] the file that needs to be uploaded
+  Future<String> updateFile({
+    required String collection,
+    required String path,
+    required Uint8List file,
+  }) async {
+    try {
+      await Supabase.instance.client.storage.from(collection).updateBinary(
+            path,
+            file,
+            fileOptions: const FileOptions(upsert: true),
+          );
+      return Supabase.instance.client.storage
+          .from(collection)
+          .getPublicUrl(path);
+    } catch (e) {
+      throw Exception('Error uploading file $e');
+    }
+  }
 }
